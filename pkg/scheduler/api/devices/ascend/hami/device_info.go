4d07e4b7fe38d9b4836e1e36317801248a42e426
james <open4pd@4paradigm.com>
2025-11-17 14:52:39 +0800

    refactor: mv the hami definition to hami dir
    Signed-off-by: james <open4pd@4paradigm.com>

--- pkg/scheduler/api/devices/ascend/hami/device_info.go
+++ pkg/scheduler/api/devices/ascend/hami/device_info.go
@@ -36,6 +36,15 @@ import (
 )
 
 const (
+	HAMiAnnotationsPrefix   = "hami.io"
+	AssignedNodeAnnotations = "hami.io/vgpu-node"
+	AssignedTimeAnnotations = "hami.io/vgpu-time"
+	BindTimeAnnotations     = "hami.io/bind-time"
+	DeviceBindPhase         = "hami.io/bind-phase"
+	DeviceBindAllocating    = "allocating"
+	DeviceBindFailed        = "failed"
+	DeviceBindSuccess       = "success"
+
 	Ascend910Prefix        = "Ascend910"
 	Ascend910NetworkWeight = 10
 	// binpack means the lower device memory remained after this allocation, the better
--- pkg/scheduler/api/devices/util.go
+++ pkg/scheduler/api/devices/util.go
@@ -67,17 +67,6 @@ const (
 	Skip
 )
 
-const (
-	HAMiAnnotationsPrefix   = "hami.io"
-	AssignedNodeAnnotations = "hami.io/vgpu-node"
-	AssignedTimeAnnotations = "hami.io/vgpu-time"
-	BindTimeAnnotations     = "hami.io/bind-time"
-	DeviceBindPhase         = "hami.io/bind-phase"
-	DeviceBindAllocating    = "allocating"
-	DeviceBindFailed        = "failed"
-	DeviceBindSuccess       = "success"
-)
-
 var kubeClient *kubernetes.Clientset
 
 func GetClient() kubernetes.Interface {
